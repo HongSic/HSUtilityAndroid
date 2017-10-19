8776288de04ae2b09a65ea19ec0039e2ca844749
ParkHongSic <gpo04174@naver.com>
2017-10-20 00:31:51 +0900

    171020 Update the Gesture/HSGestureEventArgs.java

--- src/main/java/hs/utility/android/Gesture/HSGestureEventArgs.java
+++ src/main/java/hs/utility/android/Gesture/HSGestureEventArgs.java
@@ -3,7 +3,7 @@ package hs.utility.android.Gesture;
 import java.util.EnumSet;
 import java.util.Set;
 
-import hs.utility.Event.HSEventArgs;
+import kr.arumnarae.finder.Libs.HSUtillity.Event.HSEventArgs;
 
 /**
  * Created by ParkHongSic on 2016-09-03.
@@ -13,11 +13,25 @@ public class HSGestureEventArgs extends HSEventArgs {
 
     private Set<HSGestureSwypeEnum> SwypePosition = EnumSet.of(HSGestureSwypeEnum.UNKNOWN);
 
-    public HSGestureEventArgs(EnumSet SwypePosition) {
-        this.SwypePosition = SwypePosition;
+    public HSGestureEventArgs(EnumSet SwypePosition) {this.SwypePosition = SwypePosition;  }
+
+    public Set<HSGestureSwypeEnum> getSwype() {
+        return SwypePosition;
     }
 
-    public Set<HSGestureSwypeEnum> getSwypePosition() {
-        return SwypePosition;
+    public HSGestureSwypeEnum getSwypeAction()
+    {
+        if(SwypePosition.contains(HSGestureSwypeEnum.FLING))return HSGestureSwypeEnum.FLING;
+        else if(SwypePosition.contains(HSGestureSwypeEnum.SCROLL))return HSGestureSwypeEnum.SCROLL;
+        else return HSGestureSwypeEnum.UNKNOWN;
+    }
+    public HSGestureSwypeEnum getSwypePosition()
+    {
+        for (HSGestureSwypeEnum Swype : SwypePosition)
+        {
+            if(Swype != HSGestureSwypeEnum.FLING || Swype != HSGestureSwypeEnum.SCROLL)
+                return Swype;
+        }
+        return HSGestureSwypeEnum.UNKNOWN;
     }
 }
